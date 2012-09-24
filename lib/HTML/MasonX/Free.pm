e56948bb07ea89ff3cae0c5de158a7ed735d8f62
Ricardo Signes <rjbs@cpan.org>
2012-09-24 11:19:01 -0400

    top level doc module with barely any docs

---
+++ lib/HTML/MasonX/Free.pm
@@ -0,0 +1,14 @@
+package HTML::MasonX::Free;
+use strict;
+use warnings;
+# ABSTRACT: a secret society of HTML::Mason-using throwback modules
+
+=head1 OVERVIEW
+
+These modules, together, help make HTML::Mason work more like RJBS thinks it
+should have always worked.  More documentation about the general ideas will be
+here later, when RJBS is not pressed for time.
+
+=cut
+
+1;
